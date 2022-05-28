e8ea7145d6fc4d0ca19b795159fcbac77633f62f
Philip Aipoh <72628782+Philbabb@users.noreply.github.com>
2022-05-28 11:10:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,24 +12,23 @@ class Student:
 
     def change_name(self, new_name):
         self.new_name = new_name
+        new_name = input("Enter new name: ")
         print("Student name has changed from ", self.name, " to", new_name)
         
     def change_age(self, new_age):
         self.new_age = new_age
+        new_age = input("Enter new age: ")
         print("Student age has changed from ", self.age, " to", new_age)
         
     def add_track(self, new_track):
         self.new_track = new_track
+        new_track = input("Enter new name: ")
         print("Student track has changed from ", self.track, " to", new_track)
         
     def get_score(self):
         self.get_score = self.score
         print("Student score remains ", self.score)
             
-      
-        
-  
-    
   
 Bob = Student(name="Bob", age=26, track=["FE","BE"],score=20.90)
 
